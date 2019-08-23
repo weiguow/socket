8b2260238e7d3d7e946c0e1813938d591ce4d6fb
blackLYH <474618504@qq.com>
2019-08-23 11:07:54 +0800

    fix the bugs about make client

--- make_client.sh
+++ make_client.sh
@@ -4,6 +4,6 @@
  ###
 #!bin/sh
 g++ -c jsoncpp.cpp -o jsoncpp.o
-g++ -c echo_clnt2.cpp -o echo_clnt2.o
+g++ -std=c++11 -c echo_clnt2.cpp -o echo_clnt2.o
 g++ echo_clnt2.o jsoncpp.o -o client
 rm -f *.o
